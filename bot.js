3344e1daa38ca61b488fcaf1a92bdfd845bb5190
erobororo <43712406+erobororo@users.noreply.github.com>
2018-09-30 00:46:55 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,290 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-let love = "I love you ";
+let love1 = "I love you ";
+let love2 = "i love you ";
+let love3 = "I love you, ";
+let love4 = "i love you, ";
+let love5 = "we love you "
+let love6 = "We love you ";
+let love7 = "We love you, ";
+let love8 = "we love you, ";
+
 client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(love) || message.author.bot) return;
 
-  if (message.content.startsWith(love + "mukade")) {
-    message.channel.send(".................!");
+  if (!message.content.startsWith(love1) || message.author.bot) return;
+  if (!message.content.startsWith(love2) || message.author.bot) return;
+  if (!message.content.startsWith(love3) || message.author.bot) return;
+  if (!message.content.startsWith(love4) || message.author.bot) return;
+  if (!message.content.startsWith(love5) || message.author.bot) return;
+  if (!message.content.startsWith(love6) || message.author.bot) return;
+  if (!message.content.startsWith(love7) || message.author.bot) return;
+  if (!message.content.startsWith(love8) || message.author.bot) return;
+
+
+
+  if (message.content.startsWith(love1 + "mukade")) {
+    message.reply(".................!!//");
   } else
 
-  if (message.content.startsWith(love + "muka")) {
-    message.channel.send("..................///");
+  if (message.content.startsWith(love1 + "muka")) {
+    message.reply("..................///");
   } else
+
+  if (message.content.startsWith(love1 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love1 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love1 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love1 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love1 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love1 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love2 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love2 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love2 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love2 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love2 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love2 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love2 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love2 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love3 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love3 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love3 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love3 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love3 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love3 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love3 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love3 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love4 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love4 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love4 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love4 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love4 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love4 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love4 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love4 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+if (message.content.startsWith(love5 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love5 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love5 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love5 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love5 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love5 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love5 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love5 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love6 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love6 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love6 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love6 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love6 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love6 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love6 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love6 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love7 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love7 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love7 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love7 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love7 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love7 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love7 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love7 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } else
+
+  if (message.content.startsWith(love8 + "mukade")) {
+    message.reply(".................!!//");
+  } else
+
+  if (message.content.startsWith(love8 + "muka")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love8 + "mukade-chan")) {
+    message.reply("..................///");
+  } else
+
+  if (message.content.startsWith(love8 + "Mukade")) {
+    message.reply(".................!");
+  } else
+
+  if (message.content.startsWith(love8 + "Muka")) {
+    message.reply("..................peh//");
+  } else
+
+  if (message.content.startsWith(love8 + "Mukade-chan")) {
+    message.reply("..................ah!....");
+  } else
+
+  if (message.content.startsWith(love8 + "muka-chan")) {
+    message.reply("..................//////");
+  } else
+
+  if (message.content.startsWith(love8 + "Muka-chan")) {
+    message.reply("..................?!//");
+  } 
+
+});
+
+
+
+client.on("message", (message) => {
+
 
   if (message.content.includes("peh")) {
     message.channel.send("Peh!");
@@ -56,63 +328,7 @@ client.on("message", (message) => {
 
   if (message.content.includes("arima")) {
     message.channel.send("Ｄ゛Ａ゛Ｒ゛Ｉ゛Ｍ゛Ａ゛　Ｋ゛Ｉ゛Ｊ゛Ｏ゛Ｕ゛");
-  } else
-
-  if (message.content.includes("i love you, muka")) {
-    message.reply("...............///");
-  } else
-
-  if (message.content.includes("I love you, muka")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("i love you, mukade")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("i love you, mukade-chan")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("I love you, mukade")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("I love you, mukade-chan")) {
-    message.reply("...............///");
-  } else
-
-    if (message.content.includes("I love you, Mukade-chan")) {
-    message.reply("...............///");
-  } else
-
-  if (message.content.includes("i love you muka")) {
-    message.reply("...............///");
-  } else
-
-  if (message.content.includes("I love you muka")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("i love you mukade")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("i love you mukade-chan")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("I love you mukade")) {
-    message.reply("...............///");
-  } else
-
-   if (message.content.includes("I love you mukade-chan")) {
-    message.reply("...............///");
-  } else
-
-    if (message.content.includes("I love you Mukade-chan")) {
-    message.reply("...............///");
-  }
+  } 
 
    
 });
