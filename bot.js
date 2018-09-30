e8d9d3bf4ba98bb88c44d4d0818d863ebeab23b8
erobororo <43712406+erobororo@users.noreply.github.com>
2018-09-30 00:01:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,119 +8,101 @@ client.on("ready", () => {
 client.on("message", (message) => {
   if (message.content.includes("peh")) {
     message.channel.send("Peh!");
-  }
+  } else
 
   if (message.content.includes("Peh")) {
     message.channel.send("Peh!");
-  }
+  } else
 
   if (message.content.includes("muka")) {
     message.channel.send("Peh?");
-  }
+  } else
 
   if (message.content.includes("Muka")) {
     message.channel.send("Peh?");
-  }
+  } else
 
   if (message.content.includes("fuck")) {
     message.channel.send("...");
-  }
+  } else
 
   if (message.content.includes("Fuck")) {
     message.channel.send("...");
-  }
+  } else
 
   if (message.content.includes("i love mukade")) {
     message.channel.send("...?!");
-  }
+  } else
 
   if (message.content.includes("I love mukade")) {
     message.channel.send("...?!");
-  }
+  } else
 
   if (message.content.includes("Arima")) {
     message.channel.send("Ｄ゛Ａ゛Ｒ゛Ｉ゛Ｍ゛Ａ゛　Ｋ゛Ｉ゛Ｊ゛Ｏ゛Ｕ゛");
-  }
+  } else
 
   if (message.content.includes("arima")) {
     message.channel.send("Ｄ゛Ａ゛Ｒ゛Ｉ゛Ｍ゛Ａ゛　Ｋ゛Ｉ゛Ｊ゛Ｏ゛Ｕ゛");
-  }
+  } else
 
   if (message.content.includes("i love you, muka")) {
     message.reply("...............///");
-  }
+  } else
 
   if (message.content.includes("I love you, muka")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("i love you, mukade")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("i love you, mukade-chan")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("I love you, mukade")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("I love you, mukade-chan")) {
     message.reply("...............///");
-  }
+  } else
 
     if (message.content.includes("I love you, Mukade-chan")) {
     message.reply("...............///");
-  }
+  } else
 
   if (message.content.includes("i love you muka")) {
     message.reply("...............///");
-  }
+  } else
 
   if (message.content.includes("I love you muka")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("i love you mukade")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("i love you mukade-chan")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("I love you mukade")) {
     message.reply("...............///");
-  }
+  } else
 
    if (message.content.includes("I love you mukade-chan")) {
     message.reply("...............///");
-  }
+  } else
 
     if (message.content.includes("I love you Mukade-chan")) {
     message.reply("...............///");
   }
 
-
-    if (message.substring(0, 2) == "m!") {
-        var args = message.substring(2).split(" ");
-        var cmd = args[0];
-       
-        args = args.splice(2);
-        switch(cmd) {
-
-            case "help":
-                bot.sendMessage({
-                    to: channelID,
-                    message: "Peh!? (I`m not sure how I got here and I scare easily. Try some keywords and please be nice to me.)"
-                });
-            break;
-
-        }
-
-    }
-
+   
 });
 
 
